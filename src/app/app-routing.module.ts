7e6e398465f8c22867d3fa60b2fcdf13dafbc08e
amt-augustineopokukesse <augustine.opoku-kesse@gmail.com>
2023-08-13 21:57:05 +0000

    ngrx tutorials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { CommonModule } from '@angular/common';
+
+const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  // {
+  //   path: "customers",
+  //   loadChildren: "../app/customers/customers.module#CustomersModule"
+  // }
+  {
+    path: "customers",
+    loadChildren: () => import("../app/customers/customers.module").then(m => m.CustomersModule)
+    // Use a function that returns a promise (with 'import') to correctly specify the module loading.
+  }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
